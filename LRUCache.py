a5bb6ebea73212e3a990240a84cf7da4d79ae603
deemalpatel <dp55123@yahoo.com>
2020-10-17 11:17:45 -0700

    Create LRUCache.py

---
+++ LRUCache.py
@@ -0,0 +1,34 @@
+from collections import OrderedDict
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+        
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.cache.move_to_end(key,last=False)
+            #print(self.cache)
+            return self.cache[key]
+        else:
+            #print(self.cache)
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache[key] = value
+            self.cache.move_to_end(key,last=False)
+        elif len(self.cache) < self.capacity:
+            self.cache.update({key:value})
+            self.cache.move_to_end(key,last=False)
+            #print(self.cache)
+        else:
+            self.cache.popitem()
+            self.cache.update({key:value})
+            self.cache.move_to_end(key,last=False)
+            #print(self.cache)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
